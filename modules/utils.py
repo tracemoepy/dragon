b9a77bcdf8adbd3866a163eb0b9035cccbe01259
root@localhost <>
2023-09-18 15:44:08 +0700

    chore(plugins): tidy-up

--- modules/utils.py
+++ modules/utils.py
@@ -21,7 +21,7 @@ from utils.helpers.filters import(
     onafk
 )
 from utils.format import format_exc
-from utils.helpers.downloader import download
+
 
 @Client.on_message(
     onafk
@@ -135,6 +135,67 @@ async def _profile(client: Client, message: Message):
         
     except Exception as e:
         await msg.edit(format_exc(e))
+
+
+async def download(
+    client,
+    message,
+    chatid,
+    msgid
+):
+    msg = await client.get_messages(chatid, msgid)
+    if "text" in str(msg):
+        await client.send_message(
+            message.chat.id,
+            msg.text,
+        )
+        return await message.delete()
+    file = await client.download_media(msg)
+    if "Document" in str(msg):
+        await client.send_document(
+            message.chat.id,
+            file,
+            caption=msg.caption,
+        )
+    elif "Video" in str(msg):
+        await client.send_video(
+            message.chat.id,
+            file,
+            duration=msg.video.duration,
+            width=msg.video.width,
+            height=msg.video.height,
+            caption=msg.caption,
+        )
+    elif "Animation" in str(msg):
+        await client.send_animation(
+            message.chat.id,
+            file,
+        )
+    elif "Sticker" in str(msg):
+        await client.send_sticker(
+            message.chat.id,
+            file,
+        )
+    elif "Voice" in str(msg):
+        await client.send_voice(
+            message.chat.id,
+            file,
+            caption=msg.caption,
+        )
+    elif "Audio" in str(msg):
+        await client.send_audio(
+            message.chat.id,
+            file,
+            caption=msg.caption,
+        )   
+    elif "Photo" in str(msg):
+        await client.send_photo(
+            message.chat.id,
+            file,
+            caption=msg.caption,
+        )
+    os.remove(file)
+    await message.delete()
 
 
 @Client.on_message(filters.command("copy", prefix) & filters.me)
--- utils/helpers/updater.py
+++ utils/helpers/updater.py
@@ -1,7 +1,22 @@
 import os
 import sys
 import subprocess
+
 from pyrogram import __version__
+
+
+python = f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
+pyrogram = f"{__version__}"
+
+
+DESC = f"""
+<b>Dragon-Fork</b> is a Telegram userbot based on <a href=https://github.com/Dragon-Userbot/Dragon-Userbot><b>Dragon-Userbot</b></a> (in case you didn't know, selfbot/userbot are used to automate user accounts). So how does it work? It works in a very simple way, using the pyrogram library, a python script connects to your account (creating a new session) and catches your commands.
+
+Using selfbot/userbot is against Telegram's Terms of Service, and you may get banned for using it if you're not careful.
+
+Running on <b>Python v{python}</b> with <b>Pyrogram v{pyrogram}</b>
+"""
+
 
 def restart() -> None:
     os.execvp(
@@ -24,7 +39,7 @@ def uppip() -> None:
                 "pip"
             ]
         )
-        
+
 
 def gpull() -> None:
     subprocess.run(
@@ -35,7 +50,7 @@ def gpull() -> None:
                 "-f"
             ]
         )
-        
+
 
 def upreq() -> None:
     subprocess.run(
@@ -49,16 +64,3 @@ def upreq() -> None:
                 "requirements.txt",
             ]
         )
-        
-
-
-python = f"{sys.version_info[0]}.{sys.version_info[1]}.{sys.version_info[2]}"
-pyrogram = f"{__version__}"
-
-DESC = f"""
-<b>Dragon-Fork</b> is a Telegram userbot based on <a href=https://github.com/Dragon-Userbot/Dragon-Userbot><b>Dragon-Userbot</b></a> (in case you didn't know, selfbot/userbot are used to automate user accounts). So how does it work? It works in a very simple way, using the pyrogram library, a python script connects to your account (creating a new session) and catches your commands.
-
-Using selfbot/userbot is against Telegram's Terms of Service, and you may get banned for using it if you're not careful.
-
-Running on <b>Python v{python}</b> with <b>Pyrogram v{pyrogram}</b>
-"""
